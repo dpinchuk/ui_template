30c928ccb75f0e01ea6def164fb39f5dfd3f9491
dpinchuk <pinchuk.dap@gmail.com>
2020-09-01 20:55:35 +0300

    first commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.InitWebDriver;
+
+public class BaseTest {
+
+    private InitWebDriver init = new InitWebDriver();
+
+    @BeforeMethod
+    public void setUp() {
+        init.init();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (InitWebDriver.driver != null) {
+            InitWebDriver.driver.manage().deleteAllCookies();
+            InitWebDriver.driver.quit();
+        }
+    }
+
+}
